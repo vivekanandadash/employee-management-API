8cc3eefe821efe685c09e679e9c0fcaa31c57063
Vivekananda <vivekanandadash245@gmail.com>
2026-02-02 23:29:19 +0530

    Adding Employee And EmployeeDto in CRUD-API

---
+++ src/main/java/com/example/dto/EmployeeDto.java
@@ -0,0 +1,58 @@
+package com.example.dto;
+
+public class EmployeeDto {
+    private Long id;
+    private String name;
+    private String emailId;
+    private String number;
+    private String department;
+    private Integer salary;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+}
---
+++ src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,79 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String emailId;
+
+    @Column(name = "number", nullable = false)
+    private String number;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private Integer salary;
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+//    private String name;
+//    private String emailId;
+//    private String mobileNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
