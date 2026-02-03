85302988a56dee86a32d39d36a3098053bd559f3
Vivekananda <vivekanandadash245@gmail.com>
2026-02-03 23:35:09 +0530

    Creating GlobalExceptionHandler and creating Custome exception class

---
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public String globalExceptionHandler(
+            Exception e,
+            WebRequest request
+    ){
+        return "";
+
+    }
+}
